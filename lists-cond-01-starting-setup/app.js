4f4174f054aca20464262427241a88e8bf1c6a49
Magnus Karström <magnus@yesweb.se>
2022-07-19 00:12:56 +0200

    Fully working list add/remover

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,14 +1,18 @@
 const app = Vue.createApp({
   data() {
-    return { 
+    return {
       enteredGoalValue: '',
-      goals: [] };
+      goals: [],
+    };
   },
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue);
-    }
-  }
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
+    },
+  },
 });
 
 app.mount('#user-goals');
